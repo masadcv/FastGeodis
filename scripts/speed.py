f6f4721c73e326175902f3c97fb766de8ecb8c3a
ReubenDo <reuben.dorent@kcl.ac.uk>
2021-08-06 17:58:58 +0100

    Adding script for speed benchmark

---
+++ scripts/speed.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: reubendo
+"""
+import dijkstra3d
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+from tqdm import tqdm
+import os
+import torch
+import FastGeodis
+
+def fastgeodis_generalised_geodesic_distance_3d(I, S, spacing, v, l_grad, l_eucl, iter):
+    return FastGeodis.generalised_geodesic3d(I, S, spacing, v, l_grad, l_eucl, iter)
+    
+if __name__ == "__main__":
+    # Reproducibility purposes
+    np.random.seed(0)
+
+    connectivity = 26
+    l_eucl = 1
+    l_grad = 1
+    spacing = [1, 0.5, 2]
+
+    
+    for N_seed in [1,10, 100, 1000]:
+        times_dijkstra3d = []
+        times_fastgeodis = []
+        score_relative = []
+        for shape in tqdm([(k,k,k) for k in range(10,210,10)]):
+
+            # Creating image
+            img = np.arange(np.prod(shape))
+            np.random.shuffle(img)
+            img = img.reshape(shape).astype(np.float32)
+            
+
+            N_seed = np.min([N_seed,np.prod(shape)-1])
+
+            # Sources (extreme points along the x axis)
+            sourcesind = np.random.choice(np.arange(np.prod(shape)), size=N_seed, replace=False)
+            sources = np.unravel_index(sourcesind, shape)
+            sources = np.stack(sources,1).tolist()
+
+            #S = np.zeros_like(img).astype(np.float32)
+            S = np.zeros_like(img, np.uint8)
+            for source in sources:
+                S[source[0]][source[1]][source[2]] = 1
+
+            
+            t = time.time()
+            field_dijkstra3d = dijkstra3d.distance_field(
+                data=img,
+                prob=np.zeros_like(img),
+                source=sources, 
+                connectivity=connectivity, 
+                spacing=spacing, 
+                l_grad=l_grad, 
+                l_eucl=l_eucl,
+                l_prob=0.0)
+            times_dijkstra3d.append(time.time() - t)
+
+            
+            It = torch.from_numpy(img).unsqueeze_(0).unsqueeze_(0).float()
+            St = torch.from_numpy(1 - S.astype(np.float32)).unsqueeze_(0).unsqueeze_(0)
+            
+            t = time.time()
+            D_raster = np.squeeze(
+                fastgeodis_generalised_geodesic_distance_3d(
+                    It, St, spacing, 1e10, l_grad, l_eucl, 4
+                ).numpy()
+            )
+            times_fastgeodis.append(time.time() - t)
+
+            score_relative.append((abs(field_dijkstra3d-D_raster)/(D_raster+1e-7)*100).mean())
+            
+        
+
+                        
+        list_x = [np.prod((k,k,k)) for k in range(10,210,10)]  
+
+        fig,ax = plt.subplots()
+
+        ax.set_title(f'Time in (s) per number of voxels - Seeds: {N_seed} random points')
+        ax.set_xlabel('Spatial size')
+        ax.set_ylabel('Execution time (seconds)')
+        ax.plot(list_x, times_fastgeodis, marker='o', color='g',  label='FastGeodesic')
+        ax.plot(list_x, times_dijkstra3d, marker='o', color='r', label='Djikstra3D')
+        ax.legend()
+
+
+        ax2=ax.twinx()
+        ax2.scatter(list_x, score_relative, marker='s', color='b', edgecolors='none', label='Mean elative error')
+        ax2.set_ylabel("Relative error (%)",color="blue")
+        #ax2.legend()
+
+        fig.tight_layout()
+        #fig.show()
+        fig.savefig(os.path.join('figures',  f'speed_{N_seed}.png'))
+        
