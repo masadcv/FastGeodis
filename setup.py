3f3cba72cb383e5f8aea0cc81e7d676d4cb293a5
masadcv <muhammad.asad@kcl.ac.uk>
2021-09-12 03:13:03 +0100

    separated cpu code into separate header/cpp file
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from torch.utils import cpp_extension
 setup(name='FastGeodis',
       ext_modules=[cpp_extension.CppExtension(
             name='FastGeodis', 
-            sources=['src/fastgeodis.cpp'],
+            sources=['src/fastgeodis.cpp', 'src/fastgeodis_cpu.cpp'],
             extra_compile_args=['-fopenmp'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
