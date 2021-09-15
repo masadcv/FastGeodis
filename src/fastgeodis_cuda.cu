b3e751abded74a79a071f1309ae769cb65e3da36
masadcv <muhammad.asad@kcl.ac.uk>
2021-09-15 17:36:36 +0100

    staging strip code for now, focusing on direct vectorised cuda impl for 3d
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>

--- src/fastgeodis_cuda.cu
+++ src/fastgeodis_cuda.cu
@@ -13,6 +13,8 @@
 
 // whether to use float* or Pytorch accessors in CUDA kernels
 #define USE_PTR 0
+// whether to use STRIP based algorithm from paper
+#define USE_STRIP 0
 
 __device__ float l1distance_cuda(const float &in1, const float &in2)
 {
@@ -30,228 +32,128 @@ float l1distance_cuda(const float *in1, const float *in2, int size)
 }
 
 
-// template <typename scalar_t>
-// __global__ void geodesic_updown_pass_kernel(
-//     torch::PackedTensorAccessor32<scalar_t, 4, torch::RestrictPtrTraits> image_ptr, 
-//     torch::PackedTensorAccessor32<scalar_t, 4, torch::RestrictPtrTraits> distance_ptr,
-//     const float &l_grad, 
-//     const float &l_eucl,
-//     const float *local_dist,
-//     const int row,
-//     const int height,
-//     const int width)
-// {
-//     __shared__ float lastRow[THREAD_COUNT+2];
-//     __shared__ float curRow[THREAD_COUNT+2];
+template <typename scalar_t>
+__global__ void geodesic_updown_pass_kernel(
+    torch::PackedTensorAccessor32<scalar_t, 4, torch::RestrictPtrTraits> image_ptr, 
+    torch::PackedTensorAccessor32<scalar_t, 4, torch::RestrictPtrTraits> distance_ptr,
+    const float l_grad, 
+    const float l_eucl,
+    const int row,
+    const int height,
+    const int width)
+{
+    __shared__ float lastRow[THREAD_COUNT+2];
+    __shared__ float curRow[THREAD_COUNT+2];
 
-//     // overlap
-//     int blockStartX = (blockIdx.x * THREAD_COUNT) - (blockIdx.x * STRIP_HEIGHT * 2);
-//     int blockSafeX = blockStartX + THREAD_COUNT - (2 * STRIP_HEIGHT);
+    const float local_dist[] = {sqrt(float(2.)), float(1.), sqrt(float(2.))};
 
-//     int kernelX = blockStartX + threadIdx.x;
+    // overlap
+    int blockStartX = (blockIdx.x * THREAD_COUNT) - (blockIdx.x * STRIP_HEIGHT * 2);
+    int blockSafeX = blockStartX + THREAD_COUNT - (2 * STRIP_HEIGHT);
 
-//     // copy last and current row into shared memory using each thread
-//     lastRow[threadIdx.x + 1] = -1;// distance_ptr[0][0][row-1][kernelX];
-//     curRow[threadIdx.x + 1] = -1;// distance_ptr[0][0][row][kernelX];
-//     if(kernelX < width)
-//     {
-//         lastRow[threadIdx.x + 1] = distance_ptr[0][0][row-1][kernelX];
-//         curRow[threadIdx.x + 1] = distance_ptr[0][0][row][kernelX];
-//     }
+    int kernelX = blockStartX + threadIdx.x;
+
+    // copy last and current row into shared memory using each thread
+    lastRow[threadIdx.x + 1] = -1;// distance_ptr[0][0][row-1][kernelX];
+    curRow[threadIdx.x + 1] = -1;// distance_ptr[0][0][row][kernelX];
+    if(kernelX < width)
+    {
+        lastRow[threadIdx.x + 1] = distance_ptr[0][0][row-1][kernelX];
+        curRow[threadIdx.x + 1] = distance_ptr[0][0][row][kernelX];
+    }
     
-//     if (threadIdx.x == 0)
-//     {
-//         lastRow[0] = -1;
-//         curRow[0] = -1;
-//         if ((kernelX - 1) >= 0 && (kernelX-1) < width)
-//         { 
-//             lastRow[0] = distance_ptr[0][0][row-1][kernelX-1];
-//             curRow[0] = distance_ptr[0][0][row][kernelX-1];
-//         }
-//     }
-//     else if(threadIdx.x == THREAD_COUNT + 1)
-//     {
-//         lastRow[threadIdx.x + 1] = -1;
-//         curRow[threadIdx.x + 1] = -1;
-//         if ((kernelX+1) >= 0 && (kernelX + 1) < width)
-//         {
-//             lastRow[threadIdx.x + 1] = distance_ptr[0][0][row-1][kernelX+1];
-//             curRow[threadIdx.x + 1] = distance_ptr[0][0][row][kernelX+1];
-//         }
-//     }
+    if (threadIdx.x == 0)
+    {
+        lastRow[0] = -1;
+        curRow[0] = -1;
+        if ((kernelX - 1) >= 0 && (kernelX-1) < width)
+        { 
+            lastRow[0] = distance_ptr[0][0][row-1][kernelX-1];
+            curRow[0] = distance_ptr[0][0][row][kernelX-1];
+        }
+    }
+    else if(threadIdx.x == THREAD_COUNT - 1)
+    {
+        lastRow[threadIdx.x + 1] = -1;
+        curRow[threadIdx.x + 1] = -1;
+        if ((kernelX+1) >= 0 && (kernelX + 1) < width)
+        {
+            lastRow[threadIdx.x + 1] = distance_ptr[0][0][row-1][kernelX+1];
+            curRow[threadIdx.x + 1] = distance_ptr[0][0][row][kernelX+1];
+        }
+    }
 
-//     __syncthreads();
+    __syncthreads();
 
-//     if (1==1)
-//     {
+    if (1==1)
+    {
         
-//         // top-down pass for each row in strip
-//         for (int i = 0; i < STRIP_HEIGHT; i++)
-//         {
-//             float solution = -1;
-//             int currentHeightIdx = row + i;
+        // top-down pass for each row in strip
+        for (int i = 0; i < STRIP_HEIGHT; i++)
+        {
+            float solution = -1;
+            int currentHeightIdx = row + i;
 
-//             if (currentHeightIdx < height and kernelX < width)
-//             {
-//                 int localKernelX = (int)threadIdx.x + 1;
-//                 // solution = curRow[localKernelX];
+            if (currentHeightIdx < height and kernelX < width)
+            {
+                int localKernelX = (int)threadIdx.x + 1;
+                float pval = image_ptr[0][0][currentHeightIdx][kernelX];
+                solution = curRow[localKernelX];
 
-//                 float pval = image_ptr[0][0][currentHeightIdx][kernelX];
-                
-//                 int w_i, w_ind;
-//                 float cur_dist;
-
-//                 // left back
-//                 w_i = 0;
-//                 w_ind = kernelX + w_i - 1;
-//                 cur_dist = lastRow[localKernelX + w_i - 1];
-//                 float left_solution=-1;
-//                 if (cur_dist >= 0 && (w_ind >= 0 && w_ind < width))
-//                 {
-//                     float qval = image_ptr[0][0][currentHeightIdx-1][w_ind];
-//                     float l_dist = abs(pval - qval);
-//                     left_solution = cur_dist + l_eucl * local_dist[w_i] + l_grad * l_dist;
-//                 }
-
-//                 // center back
-//                 w_i = 1;
-//                 w_ind = kernelX + w_i - 1;
-//                 cur_dist = lastRow[localKernelX + w_i - 1];
-//                 float center_solution=-1;
-//                 if (cur_dist >= 0 && (w_ind >= 0 && w_ind < width))
-//                 {
-//                     float qval = image_ptr[0][0][currentHeightIdx-1][w_ind];
-//                     float l_dist = abs(pval - qval);
-//                     center_solution = cur_dist + l_eucl * local_dist[w_i] + l_grad * l_dist;
-//                 }
-
-//                 // right back
-//                 w_i = 2;
-//                 w_ind = kernelX + w_i - 1;
-//                 cur_dist = lastRow[localKernelX + w_i - 1];
-//                 float right_solution=-1;
-//                 if (cur_dist >= 0 && (w_ind >= 0 && w_ind < width))
-//                 {
-//                     float qval = image_ptr[0][0][currentHeightIdx-1][w_ind];
-//                     float l_dist = abs(pval - qval);
-//                     right_solution = cur_dist + l_eucl * local_dist[w_i]; + l_grad * l_dist;
-//                 }
-
-//                 // for(int w_i = 0; w_i < 3; w_i++)
-//                 // {
-//                 //     const int w_ind = kernelX + w_i - 1;
-//                 //     const float cur_dist = lastRow[localKernelX + w_i - 1];
-//                 //     if (cur_dist >= 0 && (w_ind >= 0 && w_ind < width))
-//                 //     {
-//                 //         float qval = image_ptr[0][0][currentHeightIdx-1][w_ind];
-//                 //         float l_dist = l1distance_cuda(pval, qval);
-//                 //         float cur_solution = (cur_dist + l_eucl * local_dist[w_i] + l_grad * l_dist);
-//                 //         if(w_i == 0)
-//                 //         {
-//                 //             solution = cur_solution;
-//                 //         }
-//                 //         else
-//                 //         {
-//                 //             if(cur_solution < solution)
-//                 //             {
-//                 //                 solution = cur_solution;
-//                 //             }
-//                 //         }
-//                 //    }
-//                 // }
-//                 solution = left_solution;
-//                 if(center_solution < left_solution)
-//                 {
-//                     solution = center_solution;
-//                 }
-//                 else if(right_solution < left_solution && right_solution < center_solution)
-//                 {
-//                     solution = right_solution;
-//                 }
-//             }
+                for(int w_i = 0; w_i < 3; w_i++)
+                {
+                    const int w_ind = kernelX + w_i - 1;
+                    const float cur_dist = lastRow[localKernelX + w_i - 1];
+                    if (cur_dist >= 0 && (w_ind >= 0 && w_ind < width))
+                    {
+                        float l_dist = l1distance_cuda(pval, image_ptr[0][0][currentHeightIdx-1][w_ind]);
+                        float cur_solution = (cur_dist + l_eucl * local_dist[w_i] + l_grad * l_dist);
+                        solution = std::min(solution, cur_solution);
+                   }
+                }
+            }
             
-//             __syncthreads();
+            __syncthreads();
 
-//             // if (solution >= 0.0 && kernelX < blockSafeX && (curRow[threadIdx.x] < 0.0 || solution < curRow[threadIdx.x]))
-//             if (solution >= 0.0 && kernelX < blockSafeX)// && solution < curRow[threadIdx.x + 1])
-//             {
-//                 if(currentHeightIdx >= 0 && currentHeightIdx < height && kernelX >= 0 && kernelX < width)
-//                 {
-//                     distance_ptr[0][0][currentHeightIdx][kernelX] = solution;
-//                     // curRow[threadIdx.x + 1] = solution;
-//                 }
-//             }
+            // if (solution >= 0.0 && kernelX < blockSafeX && (curRow[threadIdx.x] < 0.0 || solution < curRow[threadIdx.x]))
+            if (solution >= 0.0 && kernelX < blockSafeX && solution < curRow[threadIdx.x + 1])
+            {
+                if(currentHeightIdx >= 0 && currentHeightIdx < height && kernelX >= 0 && kernelX < width)
+                {
+                    distance_ptr[0][0][currentHeightIdx][kernelX] = solution;
+                }
+            }
 
-//             lastRow[threadIdx.x] = curRow[threadIdx.x];
-//             curRow[threadIdx.x] = -1;
-//             if((currentHeightIdx+1) < height)
-//             {
-//                 curRow[threadIdx.x] = distance_ptr[0][0][currentHeightIdx + 1][kernelX];     
-//                 if (threadIdx.x == 0)
-//                 {
-//                     curRow[0] = -1;
-//                     if((kernelX - 1) >= 0)
-//                     {
-//                         curRow[0] = distance_ptr[0][0][currentHeightIdx + 1][kernelX - 1];
-//                     }
-//                 }
-//                 else if (threadIdx.x == THREAD_COUNT + 1)
-//                 {
-//                     curRow[THREAD_COUNT + 1] = -1;
-//                     if((kernelX + 1) < width)
-//                     {
-//                         curRow[THREAD_COUNT + 1] = distance_ptr[0][0][currentHeightIdx + 1][kernelX + 1];
-//                     }
-//                 }
-//             }
+            lastRow[threadIdx.x] = curRow[threadIdx.x];
+            curRow[threadIdx.x] = -1;
+            if((currentHeightIdx+1) < height)
+            {
+                curRow[threadIdx.x] = distance_ptr[0][0][currentHeightIdx + 1][kernelX];     
+                if (threadIdx.x == 0)
+                {
+                    curRow[0] = -1;
+                    if((kernelX - 1) >= 0)
+                    {
+                        curRow[0] = distance_ptr[0][0][currentHeightIdx + 1][kernelX - 1];
+                    }
+                }
+                else if (threadIdx.x == THREAD_COUNT + 1)
+                {
+                    curRow[THREAD_COUNT + 1] = -1;
+                    if((kernelX + 1) < width)
+                    {
+                        curRow[THREAD_COUNT + 1] = distance_ptr[0][0][currentHeightIdx + 1][kernelX + 1];
+                    }
+                }
+            }
 
-//             __syncthreads();
+            __syncthreads();
                 
-//                 // TODO: bottom up pass
-//         }
-//     }
-//     else{
-//         // top-down pass for each row in strip
-//         for (int i = 0; i < STRIP_HEIGHT; i++)
-//         {
-//             __syncthreads();
-//             int currentHeightIdx = row + i;
-//             if(currentHeightIdx >= 0 && currentHeightIdx < height && kernelX >= 0 && kernelX < width && kernelX < blockSafeX)
-//             {
-//                 distance_ptr[0][0][currentHeightIdx][kernelX] = kernelX;
-//                 // curRow[threadIdx.x + 1] = solution;
+            // TODO: bottom up pass
+        }
+    }  
 
-//             }
-
-//             lastRow[threadIdx.x] = curRow[threadIdx.x];
-//             curRow[threadIdx.x] = -1;
-//             if((currentHeightIdx+1) < height)
-//             {
-//                 curRow[threadIdx.x] = distance_ptr[0][0][currentHeightIdx + 1][kernelX];     
-//                 if (threadIdx.x == 0)
-//                 {
-//                     curRow[0] = -1;
-//                     if((kernelX - 1) >= 0)
-//                     {
-//                         curRow[0] = distance_ptr[0][0][currentHeightIdx + 1][kernelX - 1];
-//                     }
-//                 }
-//                 else if (threadIdx.x == THREAD_COUNT + 1)
-//                 {
-//                     curRow[THREAD_COUNT + 1] = -1;
-//                     if((kernelX + 1) < width)
-//                     {
-//                         curRow[THREAD_COUNT + 1] = distance_ptr[0][0][currentHeightIdx + 1][kernelX + 1];
-//                     }
-//                 }
-//             }
-
-//             __syncthreads();
-//         }
-
-//     }    
-
-// }
+}
 
 
 template <typename scalar_t>
@@ -345,7 +247,7 @@ void geodesic_updown_pass_cuda(const torch::Tensor image, torch::Tensor distance
 	int blockCountUpDown = (width + 1)/THREAD_COUNT + 1;
 
     int direction;
-
+    
     // top-down
     direction = -1; 
     for (int h = 1; h < height; h++)
@@ -412,42 +314,79 @@ void geodesic_updown_pass_cuda(const torch::Tensor image, torch::Tensor distance
     }
 }
 
+void geodesic_updown_pass_cuda_strip(const torch::Tensor image, torch::Tensor distance, const float &l_grad,  const float &l_eucl)
+{
+    // batch, channel, height, width
+    const int channel = image.size(1);
+    const int height = image.size(2);
+    const int width = image.size(3);
+
+    // constexpr float local_dist[] = {sqrt(2.), 1., sqrt(2.)};
+    // const float local_dist[] = {sqrt(float(2.)), float(1.), sqrt(float(2.))};
+
+    int blockSafeZone = (THREAD_COUNT - (STRIP_HEIGHT * 2));
+	int blockCountUpDown = width / blockSafeZone;
+	int blockCountLeftRight = height / blockSafeZone;
+
+	if (width % blockSafeZone != 0)
+		blockCountUpDown++;
+	if (height % blockSafeZone != 0)
+		blockCountLeftRight++;
+
+    // process each strip
+    for (int row = 1; row < height; row += STRIP_HEIGHT)
+    {
+        // call kernel
+        AT_DISPATCH_FLOATING_TYPES(image.type(), "geodesic_updown_pass_kernel_call", ([&] {
+            geodesic_updown_pass_kernel<scalar_t><<<blockCountUpDown, THREAD_COUNT>>>(
+                image.packed_accessor32<scalar_t, 4, torch::RestrictPtrTraits>(), 
+                distance.packed_accessor32<scalar_t, 4, torch::RestrictPtrTraits>(), 
+                l_grad, 
+                l_eucl, 
+                row, 
+                height, 
+                width);
+            }));
+    }
+}
+
 torch::Tensor generalised_geodesic2d_cuda(torch::Tensor &image, const torch::Tensor &mask, const float &v, const float &l_grad, const float &l_eucl, const int &iterations)
 {
+    void (*updown_pass_cuda_func)(const torch::Tensor, torch::Tensor, const float &, const float &);
     torch::Tensor distance = v * mask.clone();
     image = image.contiguous();
     distance = distance.contiguous();
 
-    // std::cout << "Reached here2" << std::endl;
-
+    updown_pass_cuda_func = USE_STRIP ? geodesic_updown_pass_cuda_strip: geodesic_updown_pass_cuda; 
+    
     // top-bottom - width*, height
-    // geodesic_updown_pass_cuda(image, distance, l_grad, l_eucl);
+    updown_pass_cuda_func(image, distance, l_grad, l_eucl);
 
-    // iteratively run the distance transform
-    for (int itr = 0; itr < iterations; itr++)
-    {
-        image = image.contiguous();
-        distance = distance.contiguous();
+    // // iteratively run the distance transform
+    // for (int itr = 0; itr < iterations; itr++)
+    // {
+    //     image = image.contiguous();
+    //     distance = distance.contiguous();
 
-        std::cout << "Reached here2" << std::endl;
+    //     std::cout << "Reached here2" << std::endl;
 
-        // top-bottom - width*, height
-        geodesic_updown_pass_cuda(image, distance, l_grad, l_eucl);
+    //     // top-bottom - width*, height
+    //     geodesic_updown_pass_cuda(image, distance, l_grad, l_eucl);
 
-        // left-right - height*, width
-        image = image.transpose(2, 3);
-        distance = distance.transpose(2, 3);
+    //     // left-right - height*, width
+    //     image = image.transpose(2, 3);
+    //     distance = distance.transpose(2, 3);
 
-        image = image.contiguous();
-        distance = distance.contiguous();
-        geodesic_updown_pass_cuda(image, distance, l_grad, l_eucl);
+    //     image = image.contiguous();
+    //     distance = distance.contiguous();
+    //     geodesic_updown_pass_cuda(image, distance, l_grad, l_eucl);
         
-        // tranpose back to original - width, height
-        image = image.transpose(2, 3);
-        distance = distance.transpose(2, 3);
+    //     // tranpose back to original - width, height
+    //     image = image.transpose(2, 3);
+    //     distance = distance.transpose(2, 3);
 
-        // * indicates the current direction of pass
-    }
+    //     // * indicates the current direction of pass
+    // }
 
     return distance;
 }
