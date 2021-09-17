721ac0c06364312c518f761893132aba67fbb7b9
masadcv <muhammad.asad@kcl.ac.uk>
2021-09-17 21:56:13 +0100

    adding new benchmark figures with gpu implementation
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>

--- scripts/test_benchmark.py
+++ scripts/test_benchmark.py
@@ -29,8 +29,6 @@ def generalised_geodesic2d_raster_cpu(I, S, v, lamda, iter):
 
 @timing
 def generalised_geodesic2d_raster_gpu(I, S, v, lamda, iter):
-    I = I.to('cuda')
-    S = S.to('cuda')
     return FastGeodis.generalised_geodesic2d(I, S, v, lamda, 1-lamda, iter)
 
 @timing
@@ -43,8 +41,6 @@ def generalised_geodesic3d_raster_cpu(I, S, spacing, v, lamda, iter):
 
 @timing
 def generalised_geodesic3d_raster_gpu(I, S, spacing, v, lamda, iter):
-    I = I.to('cuda')
-    S = S.to('cuda')
     return FastGeodis.generalised_geodesic3d(I, S, spacing, v, lamda, 1-lamda, iter)
     
 func_to_test_2d = [generalised_geodesic_distance_2d, generalised_geodesic2d_raster_cpu, generalised_geodesic2d_raster_gpu]
@@ -59,15 +55,22 @@ def test2d():
     for func in func_to_test_2d:
         time_taken_dict[func.__name__] = []
         for size in sizes_to_test:
-            ab = torch.rand((1, 1, size, size))
+            image = torch.rand((1, 1, size, size))
+            seed = torch.ones((1, 1, size, size))
+            seed[:, :, size//2, size//2] = 0.0
+
+            if 'gpu' in func.__name__:
+                image = image.to('cuda').contiguous()
+                seed = seed.to('cuda').contiguous()
+            
             tic = time.time()
             for i in range(num_runs):
                 if 'cpu' in func.__name__:
-                    func(ab, ab, 10000, 1.0, 2)
+                    func(image, seed, 10000, 1.0, 2)
                 elif 'gpu' in func.__name__ and torch.cuda.is_available():
-                    func(ab, ab, 10000, 1.0, 2)
+                    func(image, seed, 10000, 1.0, 2)
                 else:
-                    func(np.squeeze(ab.numpy()), np.squeeze(ab.numpy()), 10000, 1.0, 2)
+                    func(np.squeeze(image.cpu().numpy()), np.squeeze(seed.cpu().numpy()), 10000, 1.0, 2)
 
             time_taken_dict[func.__name__].append((time.time() - tic)/num_runs)
         print()
@@ -78,21 +81,28 @@ def test3d():
     num_runs = 2
     spacing = [1.0, 1.0, 1.0]
 
-    sizes_to_test = [64*(2**0), 64*(2**1), 64*(2**2), 64*(2**3), 64*(2**4)]#, 64*(2**5), 64*(2**6)]
+    sizes_to_test = [64*(2**0), 64*(2**1), 64*(2**2), 64*(2**3)]#, 64*(2**4)]#, 64*(2**5), 64*(2**6)]
     print(sizes_to_test)
     time_taken_dict = dict()
     for func in func_to_test_3d:
         time_taken_dict[func.__name__] = []
         for size in sizes_to_test:
-            ab = torch.rand((1, 1, size, size, size))
+            image = torch.rand((1, 1, size, size, size))
+            seed = torch.ones((1, 1, size, size, size))
+            seed[:, :, size//2, size//2, size//2] = 0.0
+            
+            if 'gpu' in func.__name__:
+                image = image.to('cuda').contiguous()
+                seed = seed.to('cuda').contiguous()
+
             tic = time.time()
             for i in range(num_runs):
                 if 'cpu' in func.__name__:
-                    func(ab, ab, spacing, 10000, 1.0, 2)
+                    func(image, seed, spacing, 10000, 1.0, 2)
                 elif 'gpu' in func.__name__ and torch.cuda.is_available():
-                    func(ab, ab, spacing, 10000, 1.0, 2)
+                    func(image, seed, spacing, 10000, 1.0, 2)
                 else:
-                    func(np.squeeze(ab.numpy()), np.squeeze(ab.numpy()), spacing, 10000, 1.0, 2)
+                    func(np.squeeze(image.cpu().numpy()), np.squeeze(seed.cpu().numpy()), spacing, 10000, 1.0, 2)
 
             time_taken_dict[func.__name__].append((time.time() - tic)/num_runs)
         print()
