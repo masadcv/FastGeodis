d2c19d592d2f67deebddbb3ec413c19a28be2bf3
masadcv <muhammad.asad@kcl.ac.uk>
2022-06-24 23:30:39 +0100

    update docs
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -21,6 +21,8 @@ project = 'FastGeodis'
 copyright = '2022, Muhammad Asad'
 author = 'Muhammad Asad'
 
+# The full version, including alpha/beta/rc tags
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 
